ac76518efe1760f1b1660b21f49c90d34edc775d
Joel Hallama <jhallama@student.hive.fi>
2020-01-17 11:41:12 +0200

    fixed conversion_o.c leak

--- conversion_o.c
+++ conversion_o.c
@@ -6,7 +6,7 @@
 /*   By: jhallama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 12:04:53 by jhallama          #+#    #+#             */
-/*   Updated: 2020/01/08 12:30:53 by jhallama         ###   ########.fr       */
+/*   Updated: 2020/01/17 11:31:16 by jhallama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 #include "libft/libft.h"
 
 static void		precision_and_print(t_fields *fields, char *s, int s_len,
-		short sign_check)
+		short sign)
 {
-	if (sign_check == 1)
+	if (sign == 1)
 	{
 		s++;
 		s_len--;
@@ -37,37 +37,29 @@ static void		precision_and_print(t_fields *fields, char *s, int s_len,
 }
 
 static short	additional_sign_and_space_check(t_fields *fields, char *s,
-		int s_len, short sign_check)
+		int s_len, short sign)
 {
 	short	inc;
 
 	inc = 0;
 	if (fields->precision == -1)
 		inc = 1;
-	if (fields->plus == 1 && s[0] != '-' && (fields->min ==
-				fields->precision + inc + 1 || s_len + 1 == fields->min))
-	{
-		write(1, "+", 1);
-		fields->min--;
-		fields->result++;
-	}
-	else if (s_len < fields->min)
+	if (s_len < fields->min)
 	{
 		if (s[0] == '-' && fields->precision + inc + 1 == fields->min)
 		{
 			write(1, "-", 1);
-			sign_check = 1;
+			sign = 1;
 		}
 		else
 			write(1, " ", 1);
 		fields->min--;
 		fields->result++;
 	}
-	return (sign_check);
+	return (sign);
 }
 
-static short	left_padding(t_fields *fields, char *s, int s_len,
-		int sign_check)
+static short	left_padding(t_fields *fields, char *s, int s_len, int sign)
 {
 	short	inc;
 
@@ -84,46 +76,31 @@ static short	left_padding(t_fields *fields, char *s, int s_len,
 				write(1, "0", 1);
 				fields->result++;
 			}
-		sign_check = additional_sign_and_space_check(fields, s, s_len,
-				sign_check);
+		sign = additional_sign_and_space_check(fields, s, s_len, sign);
 	}
-	return (sign_check);
+	return (sign);
 }
 
-static short	sign_and_space_check(t_fields *fields, char *s, int s_len,
-		short sign_check)
+static short	sign_check(t_fields *fields, char *s, int s_len,
+		short sign)
 {
-	if (fields->plus == 1 && s[0] != '-' && (s_len >= fields->min ||
-				fields->minus == 1 || fields->zero == 1 ||
-				fields->precision >= fields->min))
-	{
-		write(1, "+", 1);
-		fields->min--;
-		fields->result++;
-	}
-	else if (s[0] == '-' && (s_len >= fields->min || fields->minus == 1 ||
+	if (s[0] == '-' && (s_len >= fields->min || fields->minus == 1 ||
 				(fields->zero == 1 && (fields->precision >= fields->min ||
 									fields->precision == -1))))
 	{
 		write(1, "-", 1);
-		sign_check = 1;
+		sign = 1;
 		fields->min--;
 		fields->result++;
 	}
-	else if (fields->space == 1 && fields->plus == 0 && s[0] != '-')
-	{
-		write(1, " ", 1);
-		fields->min--;
-		fields->result++;
-	}
-	return (sign_check);
+	return (sign);
 }
 
 void			conversion_o(t_fields *fields)
 {
 	char	*s;
 	int		s_len;
-	short	sign_check;
+	short	sign;
 
 	if (fields->min_astr == 1)
 		fields->min = va_arg(fields->ap, int);
@@ -131,16 +108,15 @@ void			conversion_o(t_fields *fields)
 		fields->precision = va_arg(fields->ap, int);
 	s = choose_type_o(fields);
 	s_len = ft_strlen(s);
-	sign_check = 0;
-	sign_check = sign_and_space_check(fields, s, s_len, sign_check);
-	sign_check = left_padding(fields, s, s_len, sign_check);
-	precision_and_print(fields, s, s_len, sign_check);
+	sign = 0;
+	sign = sign_check(fields, s, s_len, sign);
+	sign = left_padding(fields, s, s_len, sign);
+	precision_and_print(fields, s, s_len, sign);
 	if (fields->minus == 1)
 		while (fields->min-- > 0)
 		{
 			write(1, " ", 1);
 			fields->result++;
 		}
-	if (ft_strcmp(s, "") != 0)
-		ft_strdel(&s);
+	ft_strdel(&s);
 }
